e946a468683abbb122f575e2fb44adecf67cadd0
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-11-13 17:26:29 -0500

    try fix memory issue

--- src/game/Components/PlayerInteractionComponent.hpp
+++ src/game/Components/PlayerInteractionComponent.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <Supergoon/Content/Image.hpp>
 #include <Supergoon/Primitives/Rectangle.hpp>
+#include <memory>
 namespace Supergoon {
 struct PlayerInteractionComponent {
 	RectangleF InteractionRect;
