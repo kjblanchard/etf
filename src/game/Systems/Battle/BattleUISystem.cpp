bb4616223a56011d05ce4287e1b20a04ebbcdbe2
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-12-08 06:38:11 -0500

    remove wild auto

--- src/game/Systems/Battle/BattleUISystem.cpp
+++ src/game/Systems/Battle/BattleUISystem.cpp
@@ -13,7 +13,6 @@ void Supergoon::InitializeBattleUI() {
 	auto horiGroup = new UIHorizontalLayoutGroup(panel, "battlerHoriGroup");
 	horiGroup->YSpaceBetweenElements = 16;
 	horiGroup->Offset = {9, 9};
-	auto
 	auto text = new UIText(horiGroup, "Kevin:     HP: 500 | 500    MP: 50 | 50", "battleDisplayText", 12);
 	auto text2 = new UIText(horiGroup, "Misha:    HP: 430 | 430    MP: 50 | 50", "battleDisplayText1", 12);
 	auto text3 = new UIText(horiGroup, "Quinn:    HP: 100 | 100    MP: 50 | 50", "battleDisplayText2", 12);
