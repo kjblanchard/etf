fe1b4021c85e1672cc08d8d301248b2e28a784fd
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-12-09 18:07:43 -0500

    fix issues with widgets, but struggling with nested vlgs?

--- src/game/Systems/Battle/BattleUISystem.cpp
+++ src/game/Systems/Battle/BattleUISystem.cpp
@@ -1,30 +1,32 @@
-#include <Supergoon/pch.hpp>
-#include <Systems/Battle/BattleUISystem.hpp>
-#include <Utilities/Utilities.hpp>
 #include <Supergoon/UI/UIHorizontalLayoutGroup.hpp>
 #include <Supergoon/UI/UIText.hpp>
+#include <Supergoon/UI/UIVerticalLayoutGroup.hpp>
+#include <Supergoon/pch.hpp>
+#include <Systems/Battle/BattleUISystem.hpp>
 #include <UI/Battle/BattlerDisplay.hpp>
+#include <Utilities/Utilities.hpp>
 using namespace Supergoon;
 using namespace std;
 
 void Supergoon::InitializeBattleUI() {
 	auto fullSize = Point{435, 66};
 	auto panel = CreateUIBasePanel("battleBasePanel", Point{39, 222}, fullSize);
-	auto horiGroup = new UIHorizontalLayoutGroup(panel, "battlerHoriGroup");
-	horiGroup->YSpaceBetweenElements = 16;
-	horiGroup->Offset = {9, 9};
-	auto text = new UIText(horiGroup, "Kevin:     HP: 500 | 500    MP: 50 | 50", "battleDisplayText", 12);
-	auto text2 = new UIText(horiGroup, "Misha:    HP: 430 | 430    MP: 50 | 50", "battleDisplayText1", 12);
-	auto text3 = new UIText(horiGroup, "Quinn:    HP: 100 | 100    MP: 50 | 50", "battleDisplayText2", 12);
+	auto verticalLayoutGroup = new UIVerticalLayoutGroup(panel, "battlersVerticalGroup");
+	verticalLayoutGroup->YSpaceBetweenElements = 16;
+	verticalLayoutGroup->Offset = {9, 9};
+	auto battler1Panel = new Panel(verticalLayoutGroup, "battler1Panel");
+	auto horizontalGroup = new UIHorizontalLayoutGroup(battler1Panel, "battler1horiGroup");
+	auto nameText = new UIText(horizontalGroup, "Kevin:", "battler1NameDisplayText", 12);
+	auto hpText = new UIText(horizontalGroup, "HP: 100|100", "battler1HPDisplayText", 12);
+	auto mpTExt = new UIText(horizontalGroup, "MP: 50|50", "battler1MPDisplayText", 12);
+	// auto text2 = new UIText(horiGroup, "Misha:    HP: 430 | 430    MP: 50 | 50", "battleDisplayText1", 12);
+	// auto text3 = new UIText(horiGroup, "Quinn:    HP: 100 | 100    MP: 50 | 50", "battleDisplayText2", 12);
 	// auto text = new UIText(horiGroup, "Kevin:     HP: 500 | 500    MP: 50 | 50", "battleDisplayText", 12);
 	// auto text2 = new UIText(horiGroup, "Misha:    HP: 430 | 430    MP: 50 | 50", "battleDisplayText1", 12);
 	// auto text3 = new UIText(horiGroup, "Quinn:    HP: 100 | 100    MP: 50 | 50", "battleDisplayText2", 12);
-	text->SetLayer(1);
-	text2->SetLayer(1);
-	text3->SetLayer(1);
-	// text->Bounds.W = fullSize.X - (text->Offset.X * 2);
-	// text->Bounds.H = fullSize.Y - (text->Offset.Y * 2);
-	// text->SetCenterY(true);
+	nameText->SetLayer(1);
+	hpText->SetLayer(1);
+	mpTExt->SetLayer(1);
 }
 
 void Supergoon::UpdateBattleUI() {
